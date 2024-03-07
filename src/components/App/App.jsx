86863aa94329e5e61de0d89f3a8db276b89ca647
Zach Saffrin <6954752+zsaffrin@users.noreply.github.com>
2024-03-06 19:43:35 -0500

    Set up App folder

---
+++ src/components/App/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div>
+      <h1>ZachSaffrin.com</h1>
+    </div>
+  );
+};
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { App } from './components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div>
-      <h1>ZachSaffrin.com</h1>
-    </div>
+    <App />
   </React.StrictMode>
 );
