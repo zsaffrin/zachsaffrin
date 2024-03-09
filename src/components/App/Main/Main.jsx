44c3c2ed7d07add2c55c9f024104473f1a772b47
Zach Saffrin <6954752+zsaffrin@users.noreply.github.com>
2024-03-08 21:28:02 -0500

    Use GlobalStyle component

--- src/components/App/Main/Main.jsx
+++ src/components/App/Main/Main.jsx
@@ -1,9 +1,11 @@
+import GlobalStyle from './GlobalStyle';
 import { MainHeader } from './MainHeader';
 import { Home } from './Home';
 
 const Main = () => {
   return (
     <>
+      <GlobalStyle />
       <MainHeader />
       <Home />
     </>
