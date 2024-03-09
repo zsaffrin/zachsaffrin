d7b411750a650667bccc48fee66a7e736cc65112
Zach Saffrin <6954752+zsaffrin@users.noreply.github.com>
2024-03-08 22:39:09 -0500

    Style Hero content

--- src/components/App/Main/Home/Home.jsx
+++ src/components/App/Main/Home/Home.jsx
@@ -1,16 +1,26 @@
 import styled from 'styled-components';
+import '@fontsource-variable/raleway';
+import { Page } from '../../../ui';
 
 const Hero = styled.div`
-  padding: 1em;
+  font-size: 18px;
+`;
+const BigText = styled.div`
+  font-size: 4em;
+  font-weight: bolder;
+  font-family: 'Raleway Variable';
+  text-transform: uppercase;
 `;
 
 const Home = () => {
   return (
-    <Hero>
-      <div>Hi, I&apos;m</div>
-      <div>Zach</div>
-      <div>I like building stuff with code</div>
-    </Hero>
+    <Page>
+      <Hero>
+        <div>Hi, I&apos;m</div>
+        <BigText>Zach</BigText>
+        <div>I like building stuff with code</div>
+      </Hero>
+    </Page>
   );
 };
 
