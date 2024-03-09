e0547334ad5d4e593c6a14ccd3cbbdc6409c5830
Zach Saffrin <6954752+zsaffrin@users.noreply.github.com>
2024-03-08 21:24:47 -0500

    Setup global style

---
+++ src/components/App/Main/GlobalStyle.jsx
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle(() => {
+  return `
+    html { box-sizing: border-box; }
+    *, *:before, *:after { box-sizing: inherit; }
+
+    body {
+      background: #eee;
+      color: #111;
+      font-size: 16px;
+      margin: 0;
+      font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+        "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+        sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+    }
+
+    p {
+      line-height: 1.4;
+      margin: 0 0 1em;
+      max-width: 48rem;
+    }
+  `;
+});
+
+export default GlobalStyle;
